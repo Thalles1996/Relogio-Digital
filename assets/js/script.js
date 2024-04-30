d0b0f55a1524d9e8173ed5c6beb79a53783b12aa
talles1996 <thallesjc1996@gmail.com>
2024-04-30 14:09:08 -0300

    alteração da hora

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ const relogio = setInterval(function time(){
     let min = dateToday.getMinutes();
     let sec = dateToday.getSeconds();
 
-    if (min < 10) hr = '0' + hr;
+    if (hr < 10) hr = '0' + hr;
     if (min < 10) min = '0' + min;
     if (sec < 10) sec = '0' + sec;
 
